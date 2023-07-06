267aff94adcccf7ffa11e028c5f9a008e15426ad
BRUNO MOTA <bmota@WKMZTC00F8CF.global.publicisgroupe.net>
2023-07-06 17:12:45 -0300

    Corrigindo arquivo http_server.py

--- http_server.py
+++ http_server.py
@@ -3,25 +3,21 @@
 import SimpleHTTPServer
 import SocketServer
 import cgi
-import socket 
+import socket
 
 PORT=10000
 
 class ReuseAddrTCPServer(SocketServer.TCPServer):
     def server_bind(self):
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
         self.socket.bind(self.server_address)
 
-class ServerHandler(simpleHTTPServer.simpleHTTPRequestHandler):
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         self.wfile.write("<html><body><form action='/' method=POST>")
-
-        self.wfile.write("<textarea name=msg row='10' cols='100'></textarea>")
-
+        self.wfile.write("<textarea name=msg rows='10' cols='100'></textarea>")
         self.wfile.write("<br><input type='submit' name='submit'>")
-
         self.wfile.write("</form></body></html>")
         return
 
@@ -31,13 +27,14 @@ class ServerHandler(simpleHTTPServer.simpleHTTPRequestHandler):
         form = cgi.FieldStorage(
             fp=self.rfile,
             headers=self.headers,
-            environ={'REQUEST_METHOD':'POST', CONTENT_TYPE':self.headers['Content-Type'],
-            })
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
 
         for field in form.keys():
             self.wfile.write('\t%s=%s\n' % (field, form[field].value))
-        return    
+
+        return
 
 ReuseAddrTCPServer(("", PORT), ServerHandler).serve_forever()
 
-#Este código cria um servidor HTTP com as bibliotecas padrão dp Python, ouve na porta 10000 e só responde a requests HTTP do tipo Get e POST.
